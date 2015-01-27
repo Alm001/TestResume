b6fe9e7bb1c90e3cf101fd983ab2565868bac2fb
Alm001 <alexlm024@gmail.com>
2015-01-27 21:44:42 +0530

    upd

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,17 @@
-$("#main").append("Alex Louis Manickaraj Lawrence");.
+//$("#main").append("Alex Louis Manickaraj Lawrence");
+
+//var awesomethoughts;
+//awesomethoughts = "I'm Alex and I'm awesome";
+
+//console.log(awesomethoughts);
+
+//funthoughts = awesomethoughts.replace("awesome" , "fun");
+
+//console.log(funthoughts);
+
+var email = "came@udacity.com";
+
+var Newemail = email.replace("udacity", "gmail");
+
+console.log(email);
+console.log(Newemail);
